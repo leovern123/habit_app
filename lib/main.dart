6e3139e40a400b771737a2648d3f888f894193b3
leovern123 <umarcp31@gmail.com>
2025-12-20 00:17:16 +0700

    ganti nama branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uas_flutter/screen/splash_screen_2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,8 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
 
       ),
-      
+      home: SplashScreen2(),
+    debugShowCheckedModeBanner: false,
     );
   }
 }
