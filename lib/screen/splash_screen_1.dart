d615b97d13de445e262c2c8c6b7344533aa64a38
zaqimaulana <zaqimaulana72@gmail.com>
2025-12-23 02:55:06 +0700

    mengubah stateleswidget menjadi statefulwidget kemudian menambahkan fungsi untuk loading lottie

--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:uas_flutter/screen/splash_screen_2.dart';
 
-class SplashScreen1 extends StatelessWidget {
+class SplashScreen1 extends StatefulWidget {
   const SplashScreen1({super.key});
+
+  @override
+  State<SplashScreen1> createState() => _SplashScreen1State();
+}
+
+class _SplashScreen1State extends State<SplashScreen1> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 2), () {
+      if (!mounted) return;
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const MySplashScreen2()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
