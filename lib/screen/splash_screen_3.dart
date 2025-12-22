c12a2276b50322546abf73247e0c374a52164c29
Ferdiansyah76 <bagusferdiansyah750@gmail.com>
2025-12-22 23:20:59 +0700

    penambahan gambar

--- lib/screen/splash_screen_3.dart
+++ lib/screen/splash_screen_3.dart
@@ -1,1 +1,32 @@
 import 'package:flutter/material.dart';
+
+class SplashScreen3 extends StatelessWidget {
+  const SplashScreen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 50),
+            // diagram
+            Container(
+              width: 230,
+              height: 230,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blueAccent,
+                image: DecorationImage(
+                  image: AssetImage("assets/images/logo_global.jpg"),
+                  fit: BoxFit.cover,
+                  alignment: Alignment.center,
+                ),
+              ),
+            )
+          ],
+        ),        
+      ),
+    );
+  }
+}
