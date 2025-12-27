86dc6e093e3ed358c693ba387fa355b902e87d71
Rayhan Faras <ryhanfrasrmdhani@gmail.com>
2025-12-27 21:07:25 +0700

    Membuat logo bulat

--- lib/screen/splash_screen_4.dart
+++ lib/screen/splash_screen_4.dart
@@ -7,7 +7,28 @@ class SplashScreen4 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Splash Screen 4"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Splash Screen 4",
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 50),
+            Container(
+              width: 230,
+              height: 230,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.amber,
+                image: DecorationImage(
+                  image: AssetImage("assets/images/logo_global.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
