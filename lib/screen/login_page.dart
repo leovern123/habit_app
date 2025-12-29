2ab860068ce64f1ac1ba748e02e116f6dea43ced
zaqimaulana <zaqimaulana72@gmail.com>
2025-12-29 21:29:54 +0700

    menambahkan teks login

--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -5,6 +5,13 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Login Page',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
   }
 }
