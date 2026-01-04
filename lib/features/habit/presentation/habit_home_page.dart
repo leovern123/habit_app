4c69fc5c861124d9633c244d15d67d1a63f3ef25
leovern123 <umarcp31@gmail.com>
2026-01-04 10:20:42 +0700

    membuat scaffold, appbar dan backround home page

--- lib/features/habit/presentation/habit_home_page.dart
+++ lib/features/habit/presentation/habit_home_page.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
+import '../data/habit_service.dart';
+import '../model/habit_model.dart';
+
 
 class HabitHomePage extends StatelessWidget {
   const HabitHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final HabitService _service = HabitService();
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Habit Tracker')),
-      body: const Center(
-        child: Text(
-          'Berhasil Login 🎉',
-          style: TextStyle(fontSize: 20),
-        ),
+      backgroundColor: Colors.blue[50],
+      appBar: AppBar(
+        title: const Text('My Habits'),
+        backgroundColor: Colors.blue[400],
+      ),
+      body: StreamBuilder<List<Habit>>(
+        stream: _service.getHabits(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('Terjadi error: ${snapshot.error}'));
+          }
+          final habits = snapshot.data ?? [];
+          if (habits.isEmpty) {
+            return const Center(
+              child: Text(
+                'Belum ada habit.\nTekan tombol + untuk menambahkan.',
+                textAlign: TextAlign.center,
+              ),
+            );
+          }
+        }
       ),
     );
   }
