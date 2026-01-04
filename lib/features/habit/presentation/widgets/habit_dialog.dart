c08c5e58029f233381ed0a483caec263461303e3
leovern123 <umarcp31@gmail.com>
2026-01-04 17:50:36 +0700

    perbaiki habit dialog error

--- lib/features/habit/presentation/widgets/habit_dialog.dart
+++ lib/features/habit/presentation/widgets/habit_dialog.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import '../model/habit_model.dart';
+import '../../model/habit_model.dart';
 
 class HabitDialog extends StatefulWidget {
   final Habit? habit;
