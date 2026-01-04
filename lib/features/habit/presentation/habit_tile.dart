cf7ad464afb885027ba957bf635a77cd2a8d0a62
leovern123 <umarcp31@gmail.com>
2026-01-04 10:29:11 +0700

    update class habit tile

--- lib/features/habit/presentation/habit_tile.dart
+++ lib/features/habit/presentation/habit_tile.dart
@@ -5,13 +5,17 @@ class HabitTile extends StatelessWidget {
   final Habit habit;
   final bool isDone;
   final Function(bool) onChanged;
+  final VoidCallback? onDelete; // untuk tombol delete
+  final int? streak; // optional untuk streak
 
   const HabitTile({
     super.key,
     required this.habit,
     required this.isDone,
     required this.onChanged,
-  });
+    this.onDelete,
+    this.streak,
+  }); 
 
   @override
   Widget build(BuildContext context) {
