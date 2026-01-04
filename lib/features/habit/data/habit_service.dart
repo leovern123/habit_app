ad8b70ed9a5d9c91057b9f60aba40df31bcfd856
leovern123 <umarcp31@gmail.com>
2026-01-04 07:46:22 +0700

    membuat class habitservice

---
+++ lib/features/habit/data/habit_service.dart
@@ -0,0 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../model/habit_model.dart';
+import '../../../core/utils/date_helper.dart';
+
+class HabitService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+ 
+
+}
