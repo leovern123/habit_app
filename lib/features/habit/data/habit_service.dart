1dcbb8ea030157d27667f325eb0c154130ebfee4
leovern123 <umarcp31@gmail.com>
2026-01-04 07:47:23 +0700

    habit(service) : mengambil semua habit user

--- lib/features/habit/data/habit_service.dart
+++ lib/features/habit/data/habit_service.dart
@@ -7,6 +7,19 @@ class HabitService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
- 
+  String get uid => _auth.currentUser!.uid;
+
+  /// ambil semua habit user
+  Stream<List<Habit>> getHabits() {
+    return _firestore
+        .collection('habits')
+        .where('userId', isEqualTo: uid)
+        .snapshots()
+        .map((snapshot) {
+      return snapshot.docs
+          .map((doc) => Habit.fromFirestore(doc.data(), doc.id))
+          .toList();
+    });
+  }
 
 }
