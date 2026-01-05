2d5e74d94a68381ec6c85a548638271949bb3966
leovern123 <umarcp31@gmail.com>
2026-01-05 23:31:02 +0700

    inisialisasi main navigation

---
+++ lib/features/navigation/main_navigation.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
