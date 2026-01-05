0fc87b5f9e4a6e396c144e2bff42b56c53e86bf3
leovern123 <umarcp31@gmail.com>
2026-01-05 23:32:45 +0700

    siapkan placeholder halaman untuk setiap tab

--- lib/features/navigation/main_navigation.dart
+++ lib/features/navigation/main_navigation.dart
@@ -9,3 +9,14 @@ class MainNavigation extends StatefulWidget {
 
 class _MainNavigationState extends State<MainNavigation> {
   int _currentIndex = 0;
+
+   final List<Widget> _pages = const [
+    _PlaceholderPage(title: 'Dashboard'),
+    _PlaceholderPage(title: 'Habit List'),
+    _PlaceholderPage(title: 'Statistik'),
+    _PlaceholderPage(title: 'Profile'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
