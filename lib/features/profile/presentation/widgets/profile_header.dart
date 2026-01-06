30f213d13d408758ca0f54c596ab6dd3f38c7be7
zaqimaulana <zaqimaulana72@gmail.com>
2026-01-07 00:13:51 +0700

    menambahkan kodingan  profile header widget

--- lib/features/profile/presentation/widgets/profile_header.dart
+++ lib/features/profile/presentation/widgets/profile_header.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
 
 class ProfileHeader extends StatelessWidget {
-  const ProfileHeader({super.key});
+  final String name;
+  final String email;
+
+  const ProfileHeader({
+    super.key,
+    required this.name,
+    required this.email,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.green,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: const Icon(Icons.person, color: Colors.white, size: 40),
+          ),
+          const SizedBox(width: 16),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                name,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                email,
+                style: const TextStyle(color: Colors.white70),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
