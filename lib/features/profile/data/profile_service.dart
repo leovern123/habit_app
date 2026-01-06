23290299215bd2115e7bb2fab6665b6ce3f8ddd2
zaqimaulana <zaqimaulana72@gmail.com>
2026-01-07 00:03:28 +0700

    membuat fungsi profil service

--- lib/features/profile/data/profile_service.dart
+++ lib/features/profile/data/profile_service.dart
@@ -1,1 +1,11 @@
- 
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ProfileService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User? get currentUser => _auth.currentUser;
+
+  Future<void> logout() async {
+    await _auth.signOut();
+  }
+}
