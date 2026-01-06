c6ee537faa259830e3025aa9ab6d3d0d4cac1fa6
zaqimaulana <zaqimaulana72@gmail.com>
2026-01-07 03:17:24 +0700

    Menambahkan halaman edit profile dan mengisi nya dengan stfl

---
+++ lib/features/profile/presentation/edit_profile_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class EditProfilePage extends StatefulWidget {
+  const EditProfilePage({super.key});
+
+  @override
+  State<EditProfilePage> createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Profile'),
+      ),
+      body: const Center(
+        child: Text('Edit Profile Page Content Here'),
+      ),
+    );
+  }
+}
