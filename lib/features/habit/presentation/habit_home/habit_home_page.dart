e9705437d189e59209e9ac2dcbce9923a55c7a56
leovern123 <umarcp31@gmail.com>
2026-01-07 03:20:27 +0700

    buat kerangka habit home page baru

---
+++ lib/features/habit/presentation/habit_home/habit_home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '../../data/habit_service.dart';
+import './widgets/habit_home_header.dart';
+import './widgets/habit_today_list.dart';
+
+class HabitHomePage extends StatelessWidget {
+  const HabitHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final habitService = HabitService();
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          const HabitHomeHeader(),
+          Expanded(
+            child: HabitTodayList(habitService: habitService),
+          ),
+        ],
+      ),
+    );
+  }
+}
