477f8a3bbe0ca21cca53a4f9792b2f26225e0cf8
zaqimaulana <zaqimaulana72@gmail.com>
2026-01-07 03:21:41 +0700

    update tampilan halaman edit profile

--- lib/features/profile/presentation/edit_profile_page.dart
+++ lib/features/profile/presentation/edit_profile_page.dart
@@ -9,13 +9,30 @@ class EditProfilePage extends StatefulWidget {
 
 class _EditProfilePageState extends State<EditProfilePage> {
   @override
-  Widget build(BuildContext context) {
+   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Edit Profile'),
-      ),
-      body: const Center(
-        child: Text('Edit Profile Page Content Here'),
+      appBar: AppBar(title: const Text('Edit Profil')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              
+              decoration: const InputDecoration(
+                labelText: 'Nama',
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              
+              decoration: const InputDecoration(
+                labelText: 'URL Foto Profil',
+              ),
+            ),
+            const SizedBox(height: 24),
+           
+          ],
+        ),
       ),
     );
   }
