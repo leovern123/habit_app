5a625f8adc464bb1fba0635122ba19245f53177e
Ferdiansyah76 <bagusferdiansyah750@gmail.com>
2026-01-10 00:38:47 +0700

    tambah method untuk menampilkan notifikasi

--- lib/service/notification_service.dart
+++ lib/service/notification_service.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 class NotificationService {
   static final FlutterLocalNotificationsPlugin _notificationsPlugin =
       FlutterLocalNotificationsPlugin();
+
     static Future<void> init() async {
     const AndroidInitializationSettings androidSettings =
         AndroidInitializationSettings('@mipmap/ic_launcher');
@@ -20,4 +21,26 @@ class NotificationService {
       },
     );
   }
+  
+  static Future<void> showNotification({
+    required int id,
+    required String title,
+    required String body,
+    String? payload,
+  }) async {
+    const AndroidNotificationDetails androidDetails = AndroidNotificationDetails(
+      'habit_channel',
+      'Habit Notifications',
+      channelDescription: 'Pengingat habit harian',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+
+    const NotificationDetails platformDetails =
+        NotificationDetails(android: androidDetails);
+
+    await _notificationsPlugin.show(id, title, body, platformDetails, payload: payload);
+  }
 }
+
+
