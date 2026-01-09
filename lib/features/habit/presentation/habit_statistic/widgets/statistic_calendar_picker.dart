4ad0d107b6e0795460a2793632a6195317cda5fb
leovern123 <umarcp31@gmail.com>
2026-01-09 14:59:11 +0700

    tambah calender

---
+++ lib/features/habit/presentation/habit_statistic/widgets/statistic_calendar_picker.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class StatisticCalendarPicker extends StatelessWidget {
+  final Function(DateTimeRange) onSelected;
+
+  const StatisticCalendarPicker({super.key, required this.onSelected});
+
+  Future<void> _pick(BuildContext context) async {
+    final now = DateTime.now();
+    final result = await showDateRangePicker(
+      context: context,
+      firstDate: DateTime(now.year - 1),
+      lastDate: now,
+    );
+
+    if (result != null) {
+      onSelected(result);
+    }
+  }
