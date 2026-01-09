146fc4f215b199e44548487835d0729e538e51f2
leovern123 <umarcp31@gmail.com>
2026-01-09 14:48:24 +0700

    dukung 7 hari

--- lib/features/habit_statistic/widgets/statistic_date_filter.dart
+++ lib/features/habit_statistic/widgets/statistic_date_filter.dart
@@ -14,3 +14,10 @@ class StatisticDateFilter extends StatelessWidget {
     final end = start.add(const Duration(days: 1));
     return DateTimeRange(start: start, end: end);
   }
+   DateTimeRange _last7Days() {
+    final now = DateTime.now();
+    return DateTimeRange(
+      start: now.subtract(const Duration(days: 6)),
+      end: now,
+    );
+  }
