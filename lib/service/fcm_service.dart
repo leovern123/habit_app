81ade74bf71062b2917386769df480532fdae274
Ferdiansyah76 <bagusferdiansyah750@gmail.com>
2026-01-10 00:31:54 +0700

    minta izin notifikasi

--- lib/service/fcm_service.dart
+++ lib/service/fcm_service.dart
@@ -5,3 +5,10 @@ import 'notification_service.dart';
 class FcmService {
   static Future<void> initFCM() async {
     FirebaseMessaging messaging = FirebaseMessaging.instance;
+    
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+    log('Izin notifikasi: ${settings.authorizationStatus}');
