5f78e388a045c5486212e693966433fbd7490ab8
Ferdiansyah76 <bagusferdiansyah750@gmail.com>
2026-01-10 00:16:40 +0700

    buat class notificicationservice

---
+++ lib/service/notification_service.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter/material.dart';
+
+class NotificationService {
+  static final FlutterLocalNotificationsPlugin _notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
