4c2627dabaa5555ffb866e5fbe811f7e43591240
Rayhanfaras <ryhanfrasrmdhani@gmail.com>
2026-01-10 01:51:01 +0700

    setup halaman profil bagus

---
+++ lib/features/profile/presentation/member_detail/profil_page_bagus.dart
@@ -0,0 +1,14 @@
+class MemberBagusPage extends StatelessWidget {
+  const MemberBagusPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Profil Mahasiswa'),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
+      ),
