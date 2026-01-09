a949fd88fea2d63009c7ada76ee7cb9e1c0104c2
Rayhanfaras <ryhanfrasrmdhani@gmail.com>
2026-01-10 01:51:54 +0700

    menambahkan foto, nama, dan kelas

--- lib/features/profile/presentation/member_detail/profil_page_bagus.dart
+++ lib/features/profile/presentation/member_detail/profil_page_bagus.dart
@@ -12,3 +12,35 @@ class MemberBagusPage extends StatelessWidget {
         foregroundColor: Colors.black,
         elevation: 0,
       ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Column(
+                children: const [
+                  CircleAvatar(
+                    radius: 55,
+                    backgroundImage:
+                        AssetImage('assets/images/member5.jpg'),
+                  ),
+                  SizedBox(height: 14),
+                  Text(
+                    'Bagus Ferdiansyah',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    'TI-4A • Teknik Informatika',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
