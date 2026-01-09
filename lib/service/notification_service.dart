b0f95247e89af21b6180e5e9397f8337fa9bfeb4
Ferdiansyah76 <bagusferdiansyah750@gmail.com>
2026-01-10 00:29:36 +0700

    update NotificationService hanya untuk Android

--- lib/service/notification_service.dart
+++ lib/service/notification_service.dart
@@ -4,3 +4,20 @@ import 'package:flutter/material.dart';
 class NotificationService {
   static final FlutterLocalNotificationsPlugin _notificationsPlugin =
       FlutterLocalNotificationsPlugin();
+    static Future<void> init() async {
+    const AndroidInitializationSettings androidSettings =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    const InitializationSettings settings =
+        InitializationSettings(android: androidSettings);
+
+    await _notificationsPlugin.initialize(
+      settings,
+      onDidReceiveNotificationResponse: (NotificationResponse response) async {
+        if (response.payload != null) {
+          debugPrint('Notifikasi diklik dengan payload: ${response.payload}');
+        }
+      },
+    );
+  }
+}
